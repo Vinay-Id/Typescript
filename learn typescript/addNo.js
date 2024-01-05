5b03fc7de337402388217a548209d9036c165649
Vinay Vishwakarma <vnypro98@gmail.com>
2024-01-05 21:35:47 +0530

    first commit

---
+++ learn typescript/addNo.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(3, 7);
+console.log("The result is: ".concat(result));
---
+++ learn typescript/addNo.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  const result = add(3, 7);
+  console.log(`The result is: ${result}`);
