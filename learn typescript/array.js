5687206194fbad5595db64f209e76b86010caaaa
Vinay Vishwakarma <vnypro98@gmail.com>
2024-01-17 21:47:24 +0530

    few more programs added

---
+++ learn typescript/array.js
@@ -0,0 +1,4 @@
+var numbers = [1, 2, 3];
+numbers.push(4);
+// numbers.push("2"); 
+var head = numbers[0];
---
+++ learn typescript/array.ts
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3]; 
+numbers.push(4); 
+// numbers.push("2"); 
+let head: number = numbers[0];
